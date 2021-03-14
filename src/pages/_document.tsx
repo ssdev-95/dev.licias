c7c202903720234b759bc28ca9be3820f77b4ed2
Salomao Souza <souza95salomao@gmail.com>
2021-03-14 13:00:33 -0400

    [ADD] Favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,7 @@ export default class MyDocument extends Document {
                 <Head>
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Red+Hat+Text:wght@400;500;700&display=swap" rel="stylesheet" />
+                    <link rel="shortcut icon" href="icons/food_icon.png" type="image/x-icon"/>
                 </Head>
                 <body>
                     <Main />
