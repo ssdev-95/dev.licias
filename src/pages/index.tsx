dfdbdec12eea6f8159ba4bed470f986560d64d65
Salomao Souza <souza95salomao@gmail.com>
2021-03-15 15:45:08 -0400

    [FIX] Barely, rsrs, mobile screen width showing menus by default

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Filters from '../components/Filters';
 import MealList from '../components/MealList';
 import Meal from '../components/Meal';
@@ -6,26 +6,53 @@ import Meal from '../components/Meal';
 import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
+  const [isMenuVisible, setIsMenuVisible] = useState(true)
+  const [screenWidth, setScreenWidth] = useState(0)
+
+  const toggleMenu = event => {
+    window.addEventListener('click', ()=>{
+      event.preventDefault()
+    })
+    setIsMenuVisible(!isMenuVisible)
+  }
+
+  useEffect(()=>{
+    setScreenWidth(window.innerWidth)
+  },[])
+
+  useEffect(()=>{
+    screenWidth>800?setIsMenuVisible(true):setIsMenuVisible(false)
+  }, [screenWidth])
   
   return (
     <div className={styles.container}>
-      <div className={styles.burger}>
-        <div className={styles.rectTop}></div>
-        <div className={styles.rectMid}></div>
-        <div className={styles.rectBot}></div>
-      </div>
+      
+          {
+          (isMenuVisible)?(
+          <div className={styles.burger} onClick={toggleMenu}>
+          <div className={styles.rotateTop}></div>
+          <div className={styles.rotateBot}></div>
+          </div>
+          ):(
+          <div className={styles.burger} onClick={toggleMenu}>
+          <div className={styles.rectTop}></div>
+          <div className={styles.rectMid}></div>
+          <div className={styles.rectBot}></div>
+          </div>)
+      
+        }
       <nav className={styles.navbar}>
         <img src="icons/food_icon.png" alt="Food icon"/>
       </nav>
-      <aside className={styles.filters}>
+      {isMenuVisible&&(<aside className={styles.filters}>
         <Filters />
-      </aside>
+      </aside>)}
       <section className={styles.mealsList}>
         <Meal/>
       </section>
-      <section className={styles.search}>
+      {isMenuVisible&&(<section className={styles.search}>
         <MealList />
-      </section>
+      </section>)}
       <footer></footer>
     </div>
   )
